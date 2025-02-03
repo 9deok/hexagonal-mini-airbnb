888f47c5ba9c4d16713ecfe58ce9efa0223e9488
sj <sj@sweettracker.co.kr>
2025-02-03 13:29:01 +0900

    first commit

---
+++ src/main/java/_deok/mini_airbnb/MiniAirbnbApplication.java
@@ -0,0 +1,13 @@
+package _deok.mini_airbnb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniAirbnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniAirbnbApplication.class, args);
+	}
+
+}
